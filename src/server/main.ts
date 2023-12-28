c12f93e8e4aeef74843b28d9621dc00263170421
Виктор Бузанов <buzanov.v@promail366.com>
2023-12-29 00:31:31 +0200

    make things easier

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,7 +2,7 @@ import { Telegraf } from "telegraf";
 import * as dotenv from "dotenv";
 import { message } from "telegraf/filters";
 import { MessageEntity } from "telegraf/types";
-const { spawn } = require("node:child_process");
+import { spawnSync } from "child_process";
 const fs = require("node:fs/promises");
 var crypto = require("crypto");
 
@@ -13,54 +13,43 @@ bot.start((ctx) => ctx.reply("Welcome"));
 bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
 bot.on(message("text"), async (ctx) => {
-  let isMention = false;
-  if (ctx.chat.type === "group" || ctx.chat.type === "supergroup") {
-    isMention =
-      ctx.message?.entities?.find((e) => e.type === "mention") !== undefined;
-  } else {
-    isMention = ctx.chat.type === "private";
-  }
-  const entity: MessageEntity.PreMessageEntity | undefined =
-    ctx.message?.entities?.find(
-      (e) => e.type === "pre"
-    ) as MessageEntity.PreMessageEntity;
-  const isPython = entity?.language === "python";
+  try {
+    let isMention = false;
+    // const entity: MessageEntity.PreMessageEntity | undefined =
+    //   ctx.message?.entities?.find(
+    //     (e) => e.type === "pre"
+    //   ) as MessageEntity.PreMessageEntity;
+    // let isPython = entity?.language === "python";
+    if (ctx.chat.type === "group" || ctx.chat.type === "supergroup") {
+      isMention =
+        ctx.message?.entities?.find((e) => e.type === "mention") !== undefined;
+      // isPython = true
+    } else {
+      isMention = ctx.chat.type === "private";
+    }
 
-  console.log(ctx.message);
+    const code = ctx.message.text.replace("@python_cu_bot", "");
 
-  const code = ctx.message.text.replace("@python_cu_bot", "");
+    if (isMention) {
+      const name = crypto.randomBytes(20).toString("hex");
+      var filename = `./${name}.py`;
+      let fh = await fs.open(filename, "a");
+      fh.write(code, 0, "utf-8");
+      await fh.close();
 
-  if (isPython && isMention) {
-    const name = crypto.randomBytes(20).toString("hex");
-    var filename = `./${name}.py`;
-    let fh = await fs.open(filename, "a");
-    fh.write(code, 0, "utf-8");
-    await fh.close();
-
-    const python3 = spawn("python3", [filename]);
-    const results: string[] = [];
-    await new Promise((resolve) => {
-      python3.stdout.on("data", (data: any) => {
-        results.push(data.toString());
-      });
-      python3.stderr.on("data", (data: any) => {
-        results.push(data.toString());
-        resolve(1);
-      });
-      python3.on("close", () => {
-        resolve(1);
-      });
-    });
-    await fs.unlink(filename);
-    ctx.replyWithHTML(
-      `<i>The work is done, ${ctx.from.first_name}!</i>\n\n<pre>${results.join(
-        "\n"
-      )}</pre>`
-    );
-  } else if (isMention) {
+      // const results: string[] = [];
+      const python3 = await spawnSync("python3", [filename], { timeout: 2000 });
+      await fs.unlink(filename);
+      const output = python3.output.toString().slice(0, 1000);
+      ctx.replyWithHTML(
+        `<i>The work is done, ${ctx.from.first_name}!</i>\n\n<pre>${output}</pre>`
+      );
+    } else {
+      // do nothing
+    }
+  } catch (e) {
     ctx.reply("Sorry, I'm machine learning engineer. I code Python!");
-  } else {
-    // do nothing
+    console.log(e);
   }
 });
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
